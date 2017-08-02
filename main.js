306fef57596a1895e6f47f56032c62fe35096709
Chad Yates <cayates1989@gmail.com>
2017-08-01 23:21:16 -0400

    another commit, i was able to get the select language to show on the form but still having some issues getting the options to display correctly

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-// The Form Data
+
 let formData = [
-  // For demonstration purposes,
-  // the first form element has been added to the HTML file as a comment
-  // compare the input in the HTML file with the contents of this first object
+  
   {
     "type": "text",
     "label": "First Name",
@@ -82,20 +80,58 @@ let formData = [
   }
 ];
 
-// HINTS:
-// As you can see, we access the first element in the array
-// with [0] and then grab the property "label" using the "." operator
+// let dataContainer = document.createElement('div');
+
+// const inputs = []
+//   for (let i=0; i < formData.length; i++){
+//     const isInput = formData[i].type === "text" || formData[i].type === "email" || formData[i].type === "tel";
+//       if (isInput){
+//         inputs.push(formData[i])
+//       }
+    
+//   }
+
+
+
+
+// const textareas = []
+//   for (i=0; i < formData.length; i++){
+//     const isSelect = formData[i].type === "select";
+//       if (isSelect){
+//         textareas.push(formData[i]);
+//       }      
+//     }
+  
+
+
+
+
+
+// const selects = []
+//     for (i=0; i < formData.length; i++){
+//       const isTextArea = formData[i].type === "textarea";
+//       if (isTextArea){
+//         selects.push(formData[i]);
+//       }
+//       }
+    
+
+
+
+
+
+
 ( function(){
-  // Select the first element from the array
+  
   let first = formData[ 0 ];
-  // Log the first object
+  
   console.log( first );
-  // Log the string "First Name"
+  
   console.log( first.label );
 } )();
 
 
-// -------- Your Code Goes Below this Line --------
+
 let dataField = document.getElementById("fields");
 
 
@@ -121,27 +157,24 @@ fields.appendChild(languageInput);
 languageInput.setAttribute("placeholder", "Select language...");     
 
 
-// let languageText = document.createElement("p");
-// languageInput.appendChild(languageText);
-// languageText.innerText = "test"
+// selectObject.add(option, [1]);
 
-
-
-// let languageOptions = document.createElement("options");
-// fields.appendChild(languageOptions);
-// languageOptions.setAttribute("English", "EN");
-
-// var newOption = document.createElement("option");
-// newOption.text = 'the options text';
-// newOption.value = 'some value if you want it';
-// daySelect.appendChild(newOption);
+// for (let i = 0; i < formData[4].options.length; i++){
+//   select.appendChild(dropdownOptions);
+// }
 
 let dropdownOptions = document.createElement("option");
 languageInput.appendChild(dropdownOptions);
-dropdownOptions.value = ("EN", "EN");
-dropdownOptions.label = ("English", "English");
-dropdownOptions.value = ("FR", "FR");
-dropdownOptions.label - ("French", "French");
+dropdownOptions.label = ("English", "Select Language");    
+
+
+
+// dropdownOptions.formData[4].options[0].value = ("EN", "EN");
+// dropdownOptions.formData[4].options[0].label = ("English", "English");
+// dropdownOptions.formData[4].options[1].value = ("FR", "FR");
+// dropdownOptions.formData[4].options[1].label = ("French", "French");
+
+
 
 
 
@@ -150,11 +183,7 @@ let commentInput = document.createElement("textarea");
 fields.appendChild(commentInput);
 commentInput.setAttribute("placeholder", "Your comment...");
 
-// let yourComment = document.createElement ("textarea")
-// fields.appendChild(yourComment);
-// // yourComment.setAttribute( "type", formData[5].type);
-// yourComment.setAttribute( "placeholder", formData[5].label);
-// // yourComment.setAttribute( "id", formData[5].id);
+
 
 let mobileInput = document.createElement("input");
 fields.appendChild(mobileInput);
@@ -169,18 +198,3 @@ homeInput.setAttribute("placeholder", "Home number");
 
 
 
-// newInput.setAttribute("type", formData[i]);
-// newInput.setAttribute("label", formData[i]);
-// newInput.setAttribute("id", formData[i]);
-// newInput.setAttribute("icon", formData[i]);
-// newInput.setAttribute("options", formData[i]);
-// let second = formData[1];
-// let third = formData[2];
-// let fourth = formData[3];
-// let fifth = formData[4];
-// let sixth = formData[5];
-// let seventh = formData[6];
-// let eighth = formData[7];
-
-
-// for (let i = 0; i < formData.length; i++);
